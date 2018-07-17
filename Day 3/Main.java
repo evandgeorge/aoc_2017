97ac66fcf3a981941f05c74e4f7b7766492b04d1
Evan George <evandgeorge@gmail.com>
2018-07-17 18:08:38 -0400

    Added functions to get input from user and added tests for them

--- Day 3/Main.java
+++ Day 3/Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 	
 	//Advent of Code 2017 Day 3
 	
 	public static void main(String[] args) {
 		
+		int squareNumber;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		do {														//parse input. If input is bad (unable to parse or less than 1) try again
+			squareNumber = getSquareNumber(scanner.nextLine());
+		} while (squareNumber < 1);
+		
+		scanner.close();
+		
 	}
 	
+	public static int getSquareNumber(String input) {				//attempt to parse string input. Return -1 if error parsing
+		int number;
+		
+		try {
+			number = Integer.parseInt(input);
+			if(number < 1)
+				System.out.println("Invalid input.");
+		} catch(NumberFormatException e) {
+			System.out.println("Invalid input.");
+			number = -1;
+		}
+		
+		return number;
+	}
 }
--- Day 3/Main_tests.java
+++ Day 3/Main_tests.java
@@ -4,6 +4,16 @@ import org.junit.jupiter.api.Test;
 
 class Main_tests {
 
+	@Test
+	void test_getSquareNumber() {
+		assertEquals(Main.getSquareNumber("264"), 264);
+		assertEquals(Main.getSquareNumber("28"), 28);
+	}
 	
-	
+	@Test
+	void test_getSquareNumber_invalidInput() {
+		assertEquals(Main.getSquareNumber(""), -1);
+		assertEquals(Main.getSquareNumber("1 3"), -1);
+	}
+
 }
