380755e850d86cde2da56a1973f7b1096ca449c4
Evan George <evandgeorge@gmail.com>
2018-07-24 19:14:28 -0400

    Finished day 4
    It's not written real well but it works and I'm done with it for now

--- Day 4/Main.java
+++ Day 4/Main.java
@@ -1,7 +1,86 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
 public class Main {
 	
+	//Advent of Coding Day 4
+	
 	public static void main(String[] args) {
+		String inputText = null;
+		
+		if(args.length == 0) {												//terminate if no arguments are passed
+			System.out.println("Pass path of table text file as argument");
+			System.exit(-1);
+		}
+		
+		try {
+			inputText = new String(Files.readAllBytes(Paths.get(args[0])));		//store contents of text file into string
+			System.out.println(getNumberOfValidPassPhrases(inputText));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 		
 	}
+
+	public static int getNumberOfValidPassPhrases(String input) {
 	
+		LinkedList<String> wordsInPhrase = new LinkedList<String>();
+		String wordBuffer = "";
+		boolean phraseIsValid = true;
+		int validPhraseCount = 0;
+		
+		for(int i = 0; i < input.length(); i++) {
+			if(input.charAt(i) == ' ') {
+				if(phraseIsValid)
+					phraseIsValid = checkValidityOfPhrase(wordsInPhrase, wordBuffer);
+				wordsInPhrase.add(wordBuffer);
+				wordBuffer = "";
+				
+				continue;
+			}
+			
+			if(input.charAt(i) == '\r') {
+				for(int j = 0; j < wordsInPhrase.size(); j++) {
+					System.out.print(wordsInPhrase.get(j) + ' ' + wordBuffer + ' ');
+				}
+				
+				System.out.print("\r\n");
+				
+				if(phraseIsValid)
+					phraseIsValid = checkValidityOfPhrase(wordsInPhrase, wordBuffer);
+				wordsInPhrase.clear();
+				wordBuffer = "";
+				
+				if(phraseIsValid)
+					validPhraseCount++;
+				
+				i++;
+				phraseIsValid = true;
+				
+				continue;
+			}
+			
+			wordBuffer += input.charAt(i);
+		}
+		
+		if(phraseIsValid)
+			phraseIsValid = checkValidityOfPhrase(wordsInPhrase, wordBuffer);
+		
+		if(phraseIsValid)
+			validPhraseCount++;
+		
+		return validPhraseCount;
+	}
+	
+	public static boolean checkValidityOfPhrase(LinkedList<String> phrase, String lastWord) {
+		
+		for(int i = 0; i < phrase.size(); i++)
+			if(phrase.get(i).equals(lastWord))
+				return false;
+		
+		return true;
+	}
 }
--- Day 4/Main_tests.java
+++ Day 4/Main_tests.java
@@ -1,7 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
 
 class Main_tests {
 
+	LinkedList<String> convertStringToListWithoutLast(String phrase) {
+		
+		LinkedList<String> list = new LinkedList<String>();
+		String wordBuffer = "";
+		
+		for(int i = 0; i < phrase.length(); i++) {
+			
+			if(phrase.charAt(i) == ' ') {
+				list.add(wordBuffer);
+				wordBuffer = "";
+			} else
+				wordBuffer += phrase.charAt(i);
+		}
+		
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		System.out.print('\n');
+		
+		return list;
+	}
+	
+	@Test
+	void test_checkValidityOfPhrase() {
+		String phrase1 = "aa bb cc dd ee"; boolean valid1 = true;
+		String phrase2 = "aa bb cc dd aa"; boolean valid2 = false;
+		String phrase3 = "aa bb cc dd aaa"; boolean valid3 = true;
+		String phrase4 = "qjs giedut mzsubkn rcbugk voxk yrlp rqxfvz kspz vxg zskp"; boolean valid4 = false;
+		
+		assertEquals(valid1, Main.checkValidityOfPhrase(convertStringToListWithoutLast(phrase1), "ee"));
+		assertEquals(valid2, Main.checkValidityOfPhrase(convertStringToListWithoutLast(phrase2), "aa"));
+		assertEquals(valid3, Main.checkValidityOfPhrase(convertStringToListWithoutLast(phrase3), "aaa"));
+		assertEquals(valid4, Main.checkValidityOfPhrase(convertStringToListWithoutLast(phrase4), "aaa"));
+	}
+	
 }
