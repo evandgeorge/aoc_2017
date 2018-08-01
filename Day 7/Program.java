0395055044ba2e42be497e7af77fe0b50992f234
Evan George <evandgeorge@gmail.com>
2018-08-01 15:55:01 -0400

    Made Program class
    each Program will represent a program in the puzzle and record its name, which programs it holds, and which program holds it

---
+++ Day 7/Program.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Program {
+
+	public String name = "";
+	public ArrayList<Program> heldPrograms = new ArrayList<Program>();
+	public Program holds;
+	
+	public Program(String name) {
+		this.name = name;
+	}
+	
+}
