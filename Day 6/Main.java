27d6a7fa841bc2d488f31da759fd11f3d5ba9863
Evan George <evandgeorge@gmail.com>
2018-07-31 23:11:52 -0400

    Made test for getBlocksPerBank() method
    getBlocksPerBanks parses string input into integer arrays of the # of blocks per memory bank

--- Day 6/Main.java
+++ Day 6/Main.java
@@ -1,7 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		// Terminate if no arguments are passed
+
+		if (args.length == 0) {
+			System.out.println("Error: Pass memory banks file to program as argument.");
+			System.exit(-1);
+		}
+
+		String input = "";
+
+		// Attempt to read file
+
+		try {
+			input = new String(Files.readAllBytes(Paths.get(args[0])));
+		} catch (IOException e) {
+			System.out.println("Error reading instructions file from path \"" + args[0] +  "\". Check path");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
 	}
 
+	public static int[] getBlocksPerBank(String input) {
+		int[] blocks = new int[16];
+		
+		return blocks;
+	}
+	
 }
--- Day 6/Main_tests.java
+++ Day 6/Main_tests.java
@@ -3,4 +3,11 @@ import org.junit.jupiter.api.Test;
 
 class Main_tests {
 
+	@Test
+	void test_getBlocksPerBank() {
+		String input = "29	2	4	0	234	3	2	3	4	0	2	0	4	54	2	9923";
+		int[] expectedOutput = new int[] {29, 2, 4, 0, 234, 3, 2, 3, 4, 0, 2, 0, 4, 54, 2, 9923};
+		assertEquals(expectedOutput, Main.getBlocksPerBank(input));
+	}
+	
 }
