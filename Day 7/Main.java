279025c629ec206682bb628e55b05da90a73e0af
Evan George <evandgeorge@gmail.com>
2018-08-01 23:29:14 -0400

    added method getPrograms and test for it
    getPrograms makes a list of the names of all the programs in the input file

--- Day 7/Main.java
+++ Day 7/Main.java
@@ -1,7 +1,65 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
 public class Main {
 
 	public static void main(String[] args) {
+		// Terminate if no arguments are passed
 
+		if (args.length == 0) {
+			System.out.println("Error: Pass memory banks file to program as argument.");
+			System.exit(-1);
+		}
+
+		String input = "";
+
+		// Attempt to read file
+
+		try {
+			input = new String(Files.readAllBytes(Paths.get(args[0])));
+		} catch (IOException e) {
+			System.out.println("Error reading instructions file from path \"" + args[0] + "\". Check path");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		System.out.println(getPrograms(input).get(1200));
 	}
-	
+
+	public static LinkedList<String> getPrograms(String input) {
+		LinkedList<String> programs = new LinkedList<String>();
+
+		String programBuffer = "";
+
+		for (int i = 0; i < input.length(); i++) {
+			char currentChar = input.charAt(i);
+
+			if (currentChar >= 'a' && currentChar <= 'z')
+				programBuffer += currentChar;
+			else {
+				programs.add(programBuffer);
+				programBuffer = "";
+
+				// skip to next name in the list
+				for (int j = i + 1; j < input.length(); j++) {
+					currentChar = input.charAt(j);
+
+					if (currentChar == '\r') { // if character is a carriage return a newline will follow, add 1 to skip both
+						i = j + 1;
+						break;
+					}
+
+					if (currentChar == '\n') { // if character is a newline, move to that index so it will pass it when i is incremented
+						i = j;
+						break;
+					}
+				}
+			}
+		}
+
+		return programs;
+	}
+
 }
--- Day 7/Main_tests.java
+++ Day 7/Main_tests.java
@@ -1,7 +1,47 @@
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
 
 class Main_tests {
+
+	@Test
+	void test_parseProgramData() {
+		String testInput = "pbga (66)\r\n" + 
+				"xhth (57)\r\n" + 
+				"ebii (61)\r\n" + 
+				"havc (66)\r\n" + 
+				"ktlj (57)\r\n" + 
+				"fwft (72) -> ktlj, cntj, xhth\r\n" + 
+				"qoyq (66)\r\n" + 
+				"padx (45) -> pbga, havc, qoyq\r\n" + 
+				"tknk (41) -> ugml, padx, fwft\r\n" + 
+				"jptl (61)\r\n" + 
+				"ugml (68) -> gyxo, ebii, jptl\r\n" + 
+				"gyxo (61)\r\n" + 
+				"cntj (57)";
+		
+		LinkedList<String> expectedOutput = new LinkedList<String>();
+		expectedOutput.add("pbga");
+		expectedOutput.add("xhth");
+		expectedOutput.add("ebii");
+		expectedOutput.add("havc");
+		expectedOutput.add("ktlj");
+		expectedOutput.add("fwft");
+		expectedOutput.add("qoyq");
+		expectedOutput.add("padx");
+		expectedOutput.add("tknk");
+		expectedOutput.add("jptl");
+		expectedOutput.add("ugml");
+		expectedOutput.add("gyxo");
+		expectedOutput.add("cntj");
+		
+		LinkedList<String> output = Main.getPrograms(testInput);
+		
+		for(int i = 0; i < expectedOutput.size(); i++) {
+			assertEquals(expectedOutput.get(i), output.get(i));
+		}
+	}
 	
 }
